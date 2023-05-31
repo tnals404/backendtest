211583e0ef9980a85f02e09ac45bb8d57cdd33bc
kimyongjin <melonfreecall@hongikit.com>
2023-05-31 19:09:57 +0900

    test

--- git_boot/src/main/java/com/example/demo/HelloController.java
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
 @Controller
 public class HelloController {
 @RequestMapping("/helloboot")
